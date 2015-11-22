e8a10e8f2b1186a264db629c48924c76616fe268
Rachel Colby <rachelcolby11@gmail.com>
2015-11-22 00:08:56 -0800

    Add create/new party action, form

--- app/controllers/parties_controller.rb
+++ app/controllers/parties_controller.rb
@@ -1,10 +1,17 @@
 class PartiesController < ApplicationController
   def new
-
+    @party = Party.new
   end
 
   def create
-
+    @party = current_user.parties.build(party_params)
+     if @party.save
+       flash[:notice] = "Your party was created."
+       redirect_to welcome_path
+     else
+       flash[:error] = "There was an error. Please try again."
+       render :new
+     end
   end  
 
   def edit
@@ -16,7 +23,11 @@ class PartiesController < ApplicationController
   end
 
   def destroy
-    
+
+  end
+
+  def party_params
+    params.require(:party).permit(:name, :date)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :parties
   get 'about' => 'welcome#about'
   get 'welcome' => 'welcome#welcome'
 
