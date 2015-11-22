ec572ad8b9d97118d84a4efc561131787562d865
Rachel Colby <rachelcolby11@gmail.com>
2015-11-21 21:27:28 -0800

    Create rough 'Welcome' view w list of parties

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,4 +4,7 @@ class WelcomeController < ApplicationController
 
   def about
   end
+
+  def welcome
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'welcome/index'
   get 'welcome/about'
+  get 'welcome/welcome'
 
   root to: 'welcome#index'
 
