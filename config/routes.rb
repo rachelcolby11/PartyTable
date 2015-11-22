9fff3cc0f63e7b90ff1175f1ecab4e3c634281ac
Rachel Colby <rachelcolby11@gmail.com>
2015-11-21 21:39:53 -0800

    Clean up Welcome routes & update path names

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
-  get 'welcome/about'
-  get 'welcome/welcome'
+
+  get 'about' => 'welcome#about'
+  get 'welcome' => 'welcome#welcome'
 
   root to: 'welcome#index'
 
