d13e678610b733dd56dc7fc8fe4e06bee5ed828b
Rachel Colby <rachelcolby11@gmail.com>
2015-11-24 17:59:13 -0800

    Add create/new task actions and form

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,32 @@
 class TasksController < ApplicationController
-  def new
 
+  def new
+   @party = Party.find(params[:party_id])
+   @task = Task.new
   end
 
   def create
+    @party = Party.find(params[:party_id])
+    @task = @party.tasks
 
-  end  
+    @task = current_user.tasks.build(task_params )
+    @task.party = @party
+    @new_task = Task.new
+
+     if @task.save
+       flash[:notice] = "Your task was created."
+       redirect_to welcome_path
+     else
+       flash[:error] = "There was an error. Please try again."
+       render :new
+     end
+  end 
+
+  # def create
+  #   @post = Post.find(params[:post_id])
+  #   @comment = @post.comments.new(comment_params)
+  #   @comment.user_id = current_user.id
+  # end
 
   def edit
 
@@ -17,6 +38,12 @@ class TasksController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description, :category, :duration, :start_time, :end_time)
   end
 
 end
