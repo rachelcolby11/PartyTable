ddc96cb590d50d256ddf2bf2a70dfd9bd0a2c597
Rachel Colby <rachelcolby11@gmail.com>
2016-02-24 23:38:06 -0800

    Duplicate tasks before dragging
    Before, when you clicked on a task to drag it, the title would be removed from the list. Now, a duplicate is created that can be dragged to the timeline planner while the task title remains on the list.

--- app/assets/javascripts/parties.js
+++ app/assets/javascripts/parties.js
@@ -1,3 +1,21 @@
 $(function() {
-  $( ".draggable" ).draggable();
+  $( ".draggable" ).draggable({
+    appendTo: "body",
+    helper: "clone"
+  });
+  $( ".droppable" ).droppable({
+    activeClass: "ui-state-default",
+    hoverClass: "ui-state-hover",
+    accept: ":not(.ui-sortable-helper)",
+    drop: function( event, ui ) {
+      $( this ).text( ui.draggable.text() ).appendTo( this );
+    }
+  }).sortable({
+    items: "li:not(.placeholder)",
+    sort: function() {
+      // gets added unintentionally by droppable interacting with sortable
+      // using connectWithSortable fixes this, but doesn't allow you to customize active/hoverClass options
+      $( this ).removeClass( "ui-state-default" );
+    }
+  });
 });
